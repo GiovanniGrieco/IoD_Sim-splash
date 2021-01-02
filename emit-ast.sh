9c85b6db6479580a9cd86aad3fbc5b938fb869f1
Giovanni Grieco <giovanni@grieco.dev>
2021-01-02 11:11:25 +0100

    Pick user input for emit-ast.sh

--- emit-ast.sh
+++ emit-ast.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
-clang -x c++ -I../IoD_Sim/Development/ns3/build/ -emit-ast -o model.pch ../IoD_Sim/Development/drone/model/constant-acceleration-drone-mobility-model.cc
+clang -x c++ \
+      -I../IoD_Sim/Development/ns3/build/ \
+      -emit-ast \
+      -o model.pch \
+      $1
