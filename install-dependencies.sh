a741116e21928578f03371abf56792c76f71b073
Giovanni Grieco <giovanni.grieco@poliba.it>
2022-04-12 14:52:57 +0000

    Support fedora/centos package install

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,20 +2,40 @@
 
 source /etc/os-release
 
-if [[ "$id" -eq "debian" ]]; then
+function detected_message() {
   echo "${NAME} has been detected in your system. Installing IoD_Sim required packages automatically."
   echo "Please insert your sudo password when requested to gain administration privileges."
+}
 
-  sudo apt install -y --no-install-recommends \
-    cmake        \
-    gcc          \
-    clang        \
-    jq           \
-    llvm-dev     \
-    libclang-dev \
-    make
+case "$ID" in
+  debian | ubuntu)
+    detected_message
+    sudo apt install -y --no-install-recommends \
+      cmake        \
+      gcc          \
+      clang        \
+      jq           \
+      llvm-dev     \
+      libclang-dev \
+      make
+    ;;
 
-else
-  echo "${NAME} is not supported by this script. Please, install IoD Sim dependencies manually by following the official guide."
-  exit 1
-fi
+  fedora | centos)
+    detected_message
+    sudo dnf install -y \
+      boost-devel   \
+      cmake         \
+      cxxopts-devel \
+      gcc           \
+      clang         \
+      jq            \
+      llvm-devel    \
+      clang-devel   \
+      make
+    ;;
+
+  *)
+    echo "${NAME} is not supported by this script. Please, install IoD Sim dependencies manually by following the official guide."
+    exit 1
+    ;;
+esac
